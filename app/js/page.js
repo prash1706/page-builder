6a924efa46bbe8f5ef15cb7a8f59eca8ef062c9c
CharleneLZS <418952312@qq.com>
2016-03-07 16:18:40 +0800

    New page behavior bug fixed.

--- app/js/page.js
+++ app/js/page.js
@@ -250,8 +250,9 @@ myPageApp.controller('SetMainCtrl', ['$scope', '$timeout', '$state', '$rootScope
       };
       $('#saveBtn').button('reset');
       $scope.showSuccess('Succeed to save the ' + $scope.currentData.name + ' page');
-      $scope.stopPro();
-      $('#addModal').modal('show');
+      $scope.stopProAsyn(function(){
+        $('#addModal').modal('show');
+      });      
     }, function(res) {
       $scope.showError('Fail to save the \'' + $scope.currentData.name + '\' page, retry please');
       $scope.stopPro();
@@ -302,11 +303,11 @@ myPageApp.controller('SetMainCtrl', ['$scope', '$timeout', '$state', '$rootScope
           $scope.currentData._rev = res.rev;
           break;
         };
-        $("#addModal").modal('show');
       };
       $('#saveBtn').button('reset');
       $scope.showSuccess('Succeed to save the ' + $scope.currentData.name + ' page');
       $scope.stopPro();
+      $("#addModal").modal('show');
     }, function(res) {
       $scope.showError('Fail to save the \'' + $scope.currentData.name + '\' page, retry please');
       $scope.stopPro();
